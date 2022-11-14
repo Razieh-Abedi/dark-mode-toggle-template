394b51aba45a4b2d4465cf059dda0f92b7fbcae5
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-14 17:44:59 +0800

    created the main article component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import data from "./data"
 import Article from "./Article";
+
 function App() {
   return (
     <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Overreacted</h1>
+          <button className="btn">Toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item)=> {
+return <Article key={item.id} {...item}/>
+        })}
+      </section>
     </main>
   );
 }
--- src/Article.jsx
+++ src/Article.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Article() {
-  return <div>Article</div>;
+function Article({title, snippet, date, length}) {
+  return <article className="post">
+    <h2>{title}</h2>
+    <div className="post-info">
+      <span>date</span>
+      <span>{length} min read</span>
+    </div>
+    <p>{snippet}</p>
+  </article>;
 }
 
 export default Article;
