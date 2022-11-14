a01586affd62f1870ff0931ff14c0005357acb43
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-14 18:33:46 +0800

    wrote the toggle colors functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import data from "./data"
+import React, { useState, useEffect } from "react";
+import data from "./data";
 import Article from "./Article";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme ");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>Overreacted</h1>
-          <button className="btn">Toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            Toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
-        {data.map((item)=> {
-return <Article key={item.id} {...item}/>
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
         })}
       </section>
     </main>
